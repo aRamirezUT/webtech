1e6b44503bf66e915f354d7ed84ffe54359a278f
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2023-10-22 17:06:55 -0500

    changed url to actual url

--- Lab8/lab8.js
+++ Lab8/lab8.js
@@ -1,44 +1,41 @@
-document.addEventListener('DOMContentLoaded', function()) {
-  function getDataFromForm() {
-    // Get the first name and last name from the input elements in the form
-    const firstName = document.querySelector('input[name="fname"]').value;
-    const lastName = document.querySelector('input[name="lname"]').value;
+function getDataFromForm() {
+  // Get the first name and last name from the input elements in the form
+  const firstName = document.querySelector('input[name="fname"]').value;
+  const lastName = document.querySelector('input[name="lname"]').value;
 
-    // Call runAjax with first name and last name as arguments
-    runAjax(firstName, lastName);
-  }
+  runAjax(firstName, lastName);
+}
 
-  function runAjax(fname, lname) {
-    // Create a new XMLHttpRequest object
-    const xhr = new XMLHttpRequest();
+function runAjax(fname, lname) {
+  // Create a new XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
 
-    // Define the URL and request method
-    const url = "./ajax.php"; // Replace with the actual URL
-    const method = "GET";
+  // Define the URL and request method
+  const url = "https://web-app-sql.azurewebsites.net/Lab8/ajax.php";
+  const method = "GET";
 
-    // Construct the request URL with first name and last name as query parameters
-    const requestUrl = `${url}?firstName=${fname}&lastName=${lname}`;
+  // Construct the request URL with first name and last name as query parameters
+  const requestUrl = `${url}?firstName=${fname}&lastName=${lname}`;
 
-    // Configure the request
-    xhr.open(method, requestUrl, true);
+  // Configure the request
+  xhr.open(method, requestUrl, true);
 
-    // Define the callback function to handle the response
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Check if the response is a string
-            if (typeof xhr.responseText === "string") {
-                // Update the content of the paragraph element with the response
-                document.getElementById("responseString").textContent = xhr.responseText;
-            }
-        } else {
-            console.error("Request failed with status:", xhr.status);
-        }
-    };
+  // Define the callback function to handle the response
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+          // Check if the response is a string
+          if (typeof xhr.responseText === "string") {
+              // Update the content of the paragraph element with the response
+              document.getElementById("responseString").textContent = xhr.responseText;
+          }
+      } else {
+          console.error("Request failed with status:", xhr.status);
+      }
+  };
 
-    // Send the AJAX request
-    xhr.send();
-  }
+  // Send the AJAX request
+  xhr.send();
+}
 
-  // Attach the button click event listener
-  document.getElementById("ajaxCall").addEventListener("click", getDataFromForm);
-});
+// Attach the button click event listener
+document.getElementById("ajaxCall").addEventListener("click", getDataFromForm);
