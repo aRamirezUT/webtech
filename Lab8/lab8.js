8c911843c1a0bade4c201d36a39da35ca49a6ab0
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2023-10-22 19:58:51 -0500

    fixes

--- Lab8/lab8.js
+++ Lab8/lab8.js
@@ -1,5 +1,5 @@
 function getDataFromForm() {
-  // Get fname and lname from the input elements in index.html
+  // get fname and lname from the input elements in index.html
   let firstName = document.querySelector('input[name="fname"]').value;
   let lastName = document.querySelector('input[name="lname"]').value;
 
@@ -7,26 +7,24 @@ function getDataFromForm() {
 }
 
 function runAjax(fname, lname) {
-  console.log("runAjax called with FirstName:", fname, "LastName:", lname);
-
 // new XMLHttpRequest object
 const xhr = new XMLHttpRequest();
 
 // xhr request
 xhr.open("GET", `./ajax.php?fname=${fname}&lname=${lname}`, true);
 
-//callback function to handle the response
+// callback function to handle the response
 xhr.onload = function() {
     if (xhr.status === 200) {
         if (typeof xhr.responseText === "string") {
             // Update the content paragraph element with the response
-            document.getElementById("responseString").textContent = xhr.responseText + " " + fname + " " + lname;
+            document.getElementById("responseString").textContent = xhr.responseText;
         }
     } else {
         console.error("Request failed with status:", xhr.status);
     }
 };
 
-//send ajax request
+// send ajax request
 xhr.send();
 }
