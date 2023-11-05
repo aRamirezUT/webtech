bb8eaf0951c3da453c4485398c6d3b6cab50c1be
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2023-11-05 10:57:07 -0600

    edited save_data to not use servicesBuilder

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,36 +1,39 @@
 <?php
 require_once 'vendor/autoload.php';
-use MicrosoftAzure\Storage\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=webappstorage4413;AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net"; // Replace with your actual connection string
 
-$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+$blobClient = BlobRestProxy::createBlobService($connectionString);
 
 $containerName = "webappstorage"; // Replace with your container name
 $blobName = "names.txt"; // Replace with your desired file name
-
-// Check if the file already exists in the blob
-$blobExists = $blobRestProxy->blobExists($containerName, $blobName);
 
 // Retrieve data from the POST request
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $data = $first_name . " " . $last_name;
 
-if ($blobExists) {
-    // If the blob exists, retrieve its current content
-    $blob = $blobRestProxy->getBlob($containerName, $blobName);
-    $existingData = stream_get_contents($blob->getContentStream());
+try {
+    // Check if the blob exists
+    if ($blobClient->doesBlobExist($containerName, $blobName)) {
+        // Append data to the existing blob
+        $existingBlob = $blobClient->getBlob($containerName, $blobName);
+        $currentContent = stream_get_contents($existingBlob->getContentStream());
 
-    // Append the new data to the existing content
-    $dataToAppend = $existingData . "\n" . $data;
-} else {
-    // If the blob doesn't exist, create a new file with the initial data
-    $dataToAppend = $data;
+        $newContent = $currentContent . $dataToAppend;
+
+        $blobClient->createBlockBlob($containerName, $blobName, $newContent);
+
+        echo "Data appended successfully!";
+    } else {
+        // If the blob does not exist, create it and add data
+        $blobClient->createBlockBlob($containerName, $blobName, $dataToAppend);
+        echo "New blob created with data!";
+    }
+} catch (ServiceException $e) {
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo "Error $code: $error_message";
 }
-
-// Upload the updated content to the blob
-$blobRestProxy->createBlockBlob($containerName, $blobName, $dataToAppend);
-
-// Redirect the user back to the "index.html" page
-header("Location: index.html");
+?>
