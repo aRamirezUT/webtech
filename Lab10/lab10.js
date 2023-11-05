71c5576892196d51010605308cdcbad32358b2d7
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2023-11-05 12:05:32 -0600

    reformatted index.html and lab10.js

--- Lab10/lab10.js
+++ Lab10/lab10.js
@@ -1,3 +1,19 @@
+function saveData() {
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData(document.getElementById("dataForm"));
+    xhr.open("POST", "save_data.php", true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("first_name").value = "";
+            document.getElementById("last_name").value = "";
+            updateSavedData();
+        }
+    };
+
+    xhr.send(formData);
+}
+
 function updateSavedData() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "show_data.php", true);
@@ -10,3 +26,6 @@ function updateSavedData() {
 
     xhr.send();
 }
+
+// Initial data retrieval
+updateSavedData();
