dba67a78f252ef46babdb5a7c36c2634439026ab
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2023-11-05 11:25:15 -0600

    added listBlobOptions and removed filter prefix

--- Lab10/show_data.php
+++ Lab10/show_data.php
@@ -2,6 +2,7 @@
 require_once 'vendor/autoload.php'; // Include the Azure SDK for PHP
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 
 // Define your Azure Blob Storage connection string
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=webappstorage4413;AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net";
@@ -15,7 +16,6 @@ $containerName = 'webappstorage';
 try {
     // List the blobs in the container
     $listBlobsOptions = new ListBlobsOptions();
-    $listBlobsOptions->setPrefix(""); // You can set a prefix to filter blobs
 
     echo "These are the blobs present in the container:<br>";
 
