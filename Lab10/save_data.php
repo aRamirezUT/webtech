f031fa8cff65a9a6f62a4e751fec0af7e50c248b
Aren Ramirez <aren.ramirez@my.utsa.edu>
2023-11-04 21:37:00 -0500

    new connection method added

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,27 +1,45 @@
 <?php
-require_once 'vendor/autoload.php'; // Include the Azure SDK for PHP
+require 'vendor/autoload.php'; // Include the Composer-generated autoload file
 
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-// Configure connection to Azure Blob Storage
-$connectionString = 'DefaultEndpointsProtocol=https;AccountName=webappstorage4413;AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net';
-$containerName = 'user-data';
+use Azure\Core\AzureKeyCredential;
+use Azure\Storage\Blobs\BlobServiceClient;
 
-$blobClient = BlobRestProxy::createBlobService($connectionString);
+function uploadBlob($accountName, $containerName, $blobName, $blobContents) {
+    // Construct the connection string from the arguments.
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName={$accountName};AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net";
+
+    // Create a BlobServiceClient using the connection string.
+    $blobServiceClient = new BlobServiceClient($connectionString);
+
+    // Get the container client.
+    $containerClient = $blobServiceClient->getContainerClient($containerName);
+
+    try {
+        // Create the container if it does not exist.
+        $containerClient->create();
+
+        // Upload text to a new block blob.
+        $blobClient = $containerClient->getBlobClient($blobName);
+        $blobClient->upload($blobContents, strlen($blobContents));
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $firstName = $_POST['first_name'];
     $lastName = $_POST['last_name'];
-
     $data = $firstName . ' ' . $lastName;
 
-    // Create a unique blob name or use a specific naming convention
+    // Define your Azure Blob Storage configuration
+    $accountName = 'webappstorage4413';
+    $containerName = 'webappstorage';
     $blobName = uniqid() . '.txt';
 
-    // Upload the data to the Blob container
-    $blobClient->createBlockBlob($containerName, $blobName, $data);
+    // Upload the data to Azure Blob Storage
+    uploadBlob($accountName, $containerName, $blobName, $data);
 
     header('Location: index.html');
 } else {
     echo "Invalid request.";
 }
-?>
--- Lab10/show_data.php
+++ Lab10/show_data.php
@@ -1,19 +1,36 @@
 <?php
-require_once 'vendor/autoload.php'; // Include the Azure SDK for PHP
+require 'vendor/autoload.php'; // Include the Composer-generated autoload file
 
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use Azure\Core\AzureKeyCredential;
+use Azure\Storage\Blobs\BlobServiceClient;
 
-$connectionString = 'DefaultEndpointsProtocol=https;AccountName=webappstorage4413;AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net';
-$containerName = 'user-data';
+function listBlobs($accountName, $containerName) {
+    // Construct the connection string from the arguments.
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName={$accountName};AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net";
 
-$blobClient = BlobRestProxy::createBlobService($connectionString);
+    // Create a BlobServiceClient using the connection string.
+    $blobServiceClient = new BlobServiceClient($connectionString);
 
-$blobs = $blobClient->listBlobs($containerName);
+    // Get the container client.
+    $containerClient = $blobServiceClient->getContainerClient($containerName);
 
+    // List blobs in the container.
+    $blobs = $containerClient->listBlobs();
+
+    return $blobs;
+}
+
+// Define your Azure Blob Storage configuration
+$accountName = 'webappstorage4413';
+$containerName = 'webappstorage';
+
+// Get the list of blobs
+$blobs = listBlobs($accountName, $containerName);
+
+// Display the data in an HTML table
 echo '<table>';
 foreach ($blobs as $blob) {
-    $data = $blobClient->getBlob($containerName, $blob->getName());
-    echo '<tr><td>' . $data->getContentStream()->getContents() . '</td></tr>';
+    $blobContents = $blob->getContent();
+    echo '<tr><td>' . $blobContents . '</td></tr>';
 }
 echo '</table>';
-?>
