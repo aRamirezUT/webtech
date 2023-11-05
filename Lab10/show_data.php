58c1d1a60ec72479b43918b0016b8d1f8ccadd51
aRamirezUT <99777095+aRamirezUT@users.noreply.github.com>
2023-11-05 11:29:08 -0600

    get content from the storage container and read it

--- Lab10/show_data.php
+++ Lab10/show_data.php
@@ -2,7 +2,6 @@
 require_once 'vendor/autoload.php'; // Include the Azure SDK for PHP
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 
 // Define your Azure Blob Storage connection string
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=webappstorage4413;AccountKey=XDhJYZCbxoXpNa1cpVVMdYEDOpt/LDnLri0bnm15SyuPawCQMvIT7u7rNvaXaqUcSEA5+1/m2x0k+AStC2yWxQ==;EndpointSuffix=core.windows.net";
@@ -12,22 +11,15 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
 
 // Define your container name
 $containerName = 'webappstorage';
+$blobName = 'names.txt'; // Specify the name of the blob you want to read
 
 try {
-    // List the blobs in the container
-    $listBlobsOptions = new ListBlobsOptions();
+    // Get the content of the blob
+    $blob = $blobClient->getBlob($containerName, $blobName);
 
-    echo "These are the blobs present in the container:<br>";
-
-    do {
-        $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-
-        foreach ($result->getBlobs() as $blob) {
-            echo $blob->getName() . "<br>";
-        }
-
-        $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-    } while ($result->getContinuationToken());
+    // Output the content to the browser
+    header('Content-Type: text/plain'); // Set the content type to plain text
+    echo stream_get_contents($blob->getContentStream());
 } catch (ServiceException $e) {
     $code = $e->getCode();
     $error_message = $e->getMessage();
